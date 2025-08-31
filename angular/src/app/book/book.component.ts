45e9d84b7a23028d2c1ed30daa692c081ee5752c
Hai Long <hailongkc1203@gmail.com>
2025-08-31 07:49:12 +0700

    Creating the Server Side +The Book List Page

---
+++ angular/src/app/book/book.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { SharedModule } from '../shared/shared.module';
+import { ListService, PagedResultDto } from '@abp/ng.core';
+import { BookDto, BookService } from '@proxy/books';
+
+@Component({
+  selector: 'app-book',
+  templateUrl: './book.component.html',
+  styleUrls: ['./book.component.css'],
+  imports: [SharedModule],
+  providers: [ListService],
+})
+export class BookComponent implements OnInit {
+  book = { items: [], totalCount: 0 } as PagedResultDto<BookDto>;
+
+  constructor(public readonly list: ListService, private bookService: BookService) {}
+
+  ngOnInit() {
+    const bookStreamCreator = query => this.bookService.getList(query);
+
+    this.list.hookToQuery(bookStreamCreator).subscribe(response => {
+      this.book = response;
+    });
+  }
+}
