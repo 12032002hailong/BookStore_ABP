8aa1767b5fb1dd5fcd765d45a1b113b7eb872b68
Hai Long <hailongkc1203@gmail.com>
2025-09-01 01:05:38 +0700

    Creating, Updating and Deleting Books

--- angular/src/app/book/book.component.ts
+++ angular/src/app/book/book.component.ts
@@ -1,25 +1,91 @@
 import { Component, OnInit } from '@angular/core';
-import { SharedModule } from '../shared/shared.module';
 import { ListService, PagedResultDto } from '@abp/ng.core';
-import { BookDto, BookService } from '@proxy/books';
+import { BookDto, BookService, bookTypeOptions } from '@proxy/books';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms'; // add this
+import { SharedModule } from '../shared/shared.module';
+import { NgbDatepickerModule } from '@ng-bootstrap/ng-bootstrap'; // add this line
+import { NgbDateNativeAdapter, NgbDateAdapter } from '@ng-bootstrap/ng-bootstrap';
+import { ConfirmationService, Confirmation } from '@abp/ng.theme.shared';
 
 @Component({
   selector: 'app-book',
   templateUrl: './book.component.html',
   styleUrls: ['./book.component.css'],
-  imports: [SharedModule],
-  providers: [ListService],
+  imports: [SharedModule, NgbDatepickerModule],
+  providers: [
+    ListService,
+    { provide: NgbDateAdapter, useClass: NgbDateNativeAdapter }, // add this line
+  ],
 })
 export class BookComponent implements OnInit {
   book = { items: [], totalCount: 0 } as PagedResultDto<BookDto>;
+  form: FormGroup;
+  selectedBook = {} as BookDto;
+  bookTypes = bookTypeOptions;
 
-  constructor(public readonly list: ListService, private bookService: BookService) {}
+  isModalOpen = false;
+
+  constructor(
+    public readonly list: ListService,
+    private bookService: BookService,
+    private fb: FormBuilder,
+    private confirmation: ConfirmationService
+  ) {}
 
   ngOnInit() {
     const bookStreamCreator = query => this.bookService.getList(query);
 
     this.list.hookToQuery(bookStreamCreator).subscribe(response => {
       this.book = response;
+    });
+  }
+  createBook() {
+    this.selectedBook = {} as BookDto;
+    this.buildForm();
+    this.isModalOpen = true;
+  }
+
+  editBook(id: string) {
+    this.bookService.get(id).subscribe(book => {
+      this.selectedBook = book;
+      this.buildForm();
+      this.isModalOpen = true;
+    });
+  }
+
+  delete(id: string) {
+    this.confirmation.warn('::AreYouSureToDelete', '::AreYouSure').subscribe(status => {
+      if (status === Confirmation.Status.confirm) {
+        this.bookService.delete(id).subscribe(() => this.list.get());
+      }
+    });
+  }
+
+  buildForm() {
+    this.form = this.fb.group({
+      name: [this.selectedBook.name || '', Validators.required],
+      type: [this.selectedBook.type || null, Validators.required],
+      publishDate: [
+        this.selectedBook.publishDate ? new Date(this.selectedBook.publishDate) : null,
+        Validators.required,
+      ],
+      price: [this.selectedBook.price || null, Validators.required],
+    });
+  }
+
+  save() {
+    if (this.form.invalid) {
+      return;
+    }
+
+    const request = this.selectedBook.id
+      ? this.bookService.update(this.selectedBook.id, this.form.value)
+      : this.bookService.create(this.form.value);
+
+    request.subscribe(() => {
+      this.isModalOpen = false;
+      this.form.reset();
+      this.list.get();
     });
   }
 }
